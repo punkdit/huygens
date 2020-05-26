769ddf6472504a3795215cf83dccacb5050194b9
Simon Burton <simon@arrowtheory.com>
2020-05-26 19:05:27 +0100

    commit edits

--- render.py
+++ render.py
@@ -1,17 +1,28 @@
 #!/usr/bin/env python3
 
+"""
+Expose a drawing api ...
+"""
+
 #from math import pi
 
+
+# ----------------------------------------------------------------------------
+# Item's go in Path's
+#
 
 class Item(object):
     def __str__(self):
         return "%s(%s)"%(self.__class__.__name__, self.__dict__)
     __repr__ = __str__
 
+    def process_cairo(self, cxt):
+        pass
 
 
 class ClosePath(Item):
-    pass
+    def process_cairo(self, cxt):
+        cxt.close_path()
 
 
 class Moveto(Item):
@@ -19,10 +30,18 @@ class Moveto(Item):
         self.x = x
         self.y = y
 
+    def process_cairo(self, cxt):
+        cxt.move_to(self.x, self.y)
+
+
 class Lineto(Item):
     def __init__(self, x, y):
         self.x = x
         self.y = y
+
+    def process_cairo(self, cxt):
+        cxt.line_to(self.x, self.y)
+
 
 class Curveto(Item):
     def __init__(self, x0, y0, x1, y1, x2, y2):
@@ -33,15 +52,28 @@ class Curveto(Item):
         self.x2 = x2
         self.y2 = y2
 
+    def process_cairo(self, cxt):
+        cxt.curve_to(self.x0, self.y0, 
+            self.x1, self.y1, self.x2, self.y2)
+
+
 class RMoveto(Item):
     def __init__(self, dx, dy):
         self.dx = dx
         self.dy = dy
 
+    def process_cairo(self, cxt):
+        cxt.rel_move_to(self.dx, self.dy)
+
+
 class RLineto(Item):
     def __init__(self, dx, dy):
         self.dx = dx
         self.dy = dy
+
+    def process_cairo(self, cxt):
+        cxt.rel_line_to(self.dx, self.dy)
+
 
 class RCurveto(Item):
     def __init__(self, dx0, dy0, dx1, dy1, dx2, dy2):
@@ -52,6 +84,11 @@ class RCurveto(Item):
         self.dx2 = dx2
         self.dy2 = dy2
 
+    def process_cairo(self, cxt):
+        cxt.rel_curve_to(self.dx0, self.dy0, 
+            self.dx1, self.dy1, self.dx2, self.dy2)
+
+
 class Arc(Item):
     def __init__(self, x, y, r, angle1, angle2):
         "angle in degrees"
@@ -61,10 +98,18 @@ class Arc(Item):
         self.angle1 = angle1
         self.angle2 = angle2
 
-class Arcn(Item):
-    pass
+    def process_cairo(self, cxt):
+        cxt.arc(self.x, self.y, self.r, 2*pi*self.angle1, 2*pi*self.angle2)
 
 
+class Arcn(Arc):
+    def process_cairo(self, cxt):
+        cxt.arc_negative(self.x, self.y, self.r, 2*pi*self.angle1, 2*pi*self.angle2)
+
+
+# ----------------------------------------------------------------------------
+# Path's : a list of Item's
+#
 
 class Path(object):
     def __init__(self, items):
