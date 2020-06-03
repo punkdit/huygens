d90b8f073b917e0e7cbb440d38b51bea1e9d0b85
Simon Burton <simon@arrowtheory.com>
2020-06-03 16:01:27 +0100

    commit edits

--- flatten.py
+++ flatten.py
@@ -1,21 +1,19 @@
 #!/usr/bin/env python3
 
 """
-Hijack cairosvg to load svg into our internal data structures.
+Interpret all transforms, thereby flattening cairo operations
+to just a few primitives, such as move_to line_to and curve_to.
 """
 
 import sys
 from math import sin, cos, pi
-
-from cairosvg.parser import Tree
-from cairosvg.surface import Surface
 
 from bruhat.argv import argv
 from bruhat.render import back
 from bruhat.render.base import Context
 
 
-class Reflector(Context):
+class Flatten(Context):
 
     def __init__(self):
         Context.__init__(self)
@@ -122,56 +120,6 @@ class Reflector(Context):
 
 
 
-class DummySurf(Surface):
-
-    def __init__(self, tree, output, dpi):
-    
-        W, H = 600., 200. # point == 1/72 inch
-
-        self.context = Reflector()
-
-        self.dpi = dpi
-
-        self._old_parent_node = self.parent_node = None
-        self.output = output
-        self.font_size = None
-
-        self.context_width = W
-        self.context_height = H
-
-        self.cursor_position = [0, 0]
-        self.cursor_d_position = [0, 0]
-        self.text_path_width = 0
-        self.stroke_and_fill = True
-
-        self.tree_cache = {(tree.url, tree.get('id')): tree}
-
-        self.markers = {}
-        self.gradients = {}
-        self.patterns = {}
-        self.masks = {}
-        self.paths = {}
-        self.filters = {}
-
-        self.map_rgba = None
-        self.map_image = None
-
-        self.draw(tree)
-    
-        #surface.finish()
-
-        self.paths = self.context.paths
-
-
-def loadsvg(name, dpi=72.):
-    assert name.endswith(".svg")
-    s = open(name).read()
-    tree = Tree(bytestring=s)
-    dummy = DummySurf(tree, None, dpi)
-    item = back.Compound(dummy.paths)
-    return item
-
-
 def test():
 
     def draw_test(cxt):
@@ -197,7 +145,7 @@ def test():
     draw_test(context)
     context.restore()
 
-    cxt = Reflector()
+    cxt = Flatten()
     draw_test(cxt)
     for path in cxt.paths:
         path.process_cairo(context)
@@ -210,20 +158,6 @@ def test():
 
 if __name__ == "__main__":
 
-    if argv.test:
-        test()
-
-    else:
-
-        name = argv.next()
-        s = open(name).read()
-        tree = Tree(bytestring=s)
-        my = DummySurf(tree, None, 72.)
-        cvs = back.Canvas(my.paths)
-        cvs.writePDFfile("output.pdf")
-
-
-
-
+    test()
 
 
