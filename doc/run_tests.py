f5df2705795249f043e71cede589df98cd55a93a
Simon Burton <simon@arrowtheory.com>
2020-06-10 13:31:23 +0100

    commit edits

--- doc/run_tests.py
+++ doc/run_tests.py
@@ -3,7 +3,6 @@
 import os
 import collections
 
-from bruhat.argv import argv
 import bruhat.render.doc
 from bruhat.render.front import Canvas, Scale
 
@@ -24,9 +23,8 @@ def run_test(func):
             cvs = Canvas()
             cvs.append(Scale(2.0))
             box.render(cvs)
-            #cvs = Canvas([Scale(2.0, 2.0)]+cvs.items)
-            cvs.writePDFfile("pic-%s.pdf" % name)
-            cvs.writeSVGfile("pic-%s.svg" % name)
+            cvs.writePDFfile("images/%s.pdf" % name)
+            cvs.writeSVGfile("images/%s.svg" % name)
             print()
         except:
             print("render failed for %r"%name)
@@ -36,54 +34,6 @@ def run_test(func):
         break
 
     return lnos
-
-def html_head(s):
-    html = """\
-<!DOCTYPE html>
-<html>
-<head>
-<link rel="stylesheet" type="text/css" href="superstyle.css"> 
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Spectral">
-<link rel="stylesheet" type="text/css" href="superstyle.css"> 
-%s
-<style>
- body {
-     font-family: 'Spectral', serif;
-     font-size: 16px;
- }
-
- .center {
-     display: block;
-     margin-left: auto;
-     margin-right: auto;
- }
-</style>
-</head>
-<body>
-<main>
-""" % s
-    return html
-
-def html_tail():
-    return """
-<hr>
-
-Copyright (c) 2018 - 2020.
-
-</main>
-
-</body>
-"""
-
-def html_style(body):
-    return "<style>\n%s\n</style>\n"%body
-
-def html_p(body):
-    return "<p>%s</p>\n"%body
-
-def html_img(name):
-    return '<p><img src="%s" class="center"></p>\n'%name
-
 
 
 def process(path, name):
@@ -104,56 +54,6 @@ def process(path, name):
     for func in funcs:
         lnos = run_test(func)
 
-    if not argv.makedoc:
-        return
-
-    from pygments.formatters import HtmlFormatter
-    
-    output = open("output.html", 'w')
-    style = HtmlFormatter().get_style_defs('.highlight') 
-    print(html_head(html_style(style)), file=output)
-
-
-    code = open(fullname).read().split('\n')
-    lnos = [(lno-1,name) for (lno,name) in lnos]
-    idx = 0
-    while idx+1 < len(lnos):
-        snip = code[lnos[idx][0]+1 : lnos[idx+1][0]]
-        highlight(snip, output)
-        print(html_img("pic-%s.svg"%lnos[idx+1][1]), file=output) # XXX D.R.Y.
-        idx += 1
-
-    print(html_tail(), file=output)
-    output.close()
-
-
-def highlight(lines, output):
-    indent = None
-    assert lines
-    for line in lines:
-        if not line.strip():
-            continue
-        i = 0
-        while line.startswith(' '*i):
-            i += 1
-        space = ' '*(i-1)
-        if indent is None or indent.startswith(space):
-            indent = space
-    assert indent is not None
-    lines = [line[len(indent):] for line in lines]
-
-    code = '\n'.join(lines)
-    #print(code)
-
-    from pygments import highlight
-    from pygments.lexers import Python3Lexer
-    from pygments.formatters import HtmlFormatter
-    
-    #code = 'print "Hello World"'
-    print(highlight(code, Python3Lexer(), HtmlFormatter()), file=output)
-    print(file=output)
-
-
 
 def main():
 
@@ -161,6 +61,7 @@ def main():
     names = os.listdir(path)
     names = [name for name in names 
         if name.endswith(".py") and name.startswith("test_")]
+    names.sort()
 
     for name in names:
         process(path, name)
