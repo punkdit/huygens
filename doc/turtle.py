61742be37be7d688282b3e32c9ac5ac04273ca5f
Simon Burton <simon@arrowtheory.com>
2022-11-08 17:11:03 +0000

    commit edits

---
+++ doc/turtle.py
@@ -0,0 +1,54 @@
+
+# ## Turtle graphics
+# 
+# Use a turtle to keep track of a path as you build it.
+
+from huygens import canvas, style, color
+from huygens.turtle import Turtle
+
+cvs = canvas.canvas()
+turtle = Turtle(cvs=cvs)
+
+n = 8
+angle = 360. / n
+R = 3.0
+for i in range(n):
+    turtle.fwd(1.*R)
+    turtle.left((1./3)*angle)
+    turtle.back(0.5*R)
+    turtle.left((1./3)*angle)
+    turtle.back(0.7*R)
+    turtle.left((1./3)*angle)
+    turtle.stroke()
+cvs
+
+# We can change the stroke style that the turtle uses
+# to a thick green line.
+
+attrs = [style.linewidth.THIck, color.rgb(0.2, 0.6, 0.2)]
+
+cvs = canvas.canvas()
+turtle = Turtle(cvs=cvs, attrs=attrs)
+
+turtle.fwd(2.)
+turtle.right(300, 1.)
+turtle.fwd(2.)
+turtle.arrow(0.4)
+turtle.stroke()
+
+cvs
+
+# Another example:
+
+cvs = canvas.canvas()
+turtle = Turtle(cvs=cvs, attrs=attrs)
+
+R = 1.0
+for i in range(24*2):
+    turtle.left(320, 0.6*R)
+    turtle.left(-60, 0.3*R)
+    turtle.right(90, 0.6*R)
+    turtle.stroke(attrs=attrs)
+
+cvs
+
