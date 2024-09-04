26f9d4e5dc79d48b69a3f57ce42e3e6c8a213e84
Simon Burton <simon@arrowtheory.com>
2024-09-04 11:12:49 +0100

    progress ...

--- huygens/bicategory.py
+++ huygens/bicategory.py
@@ -1,5 +1,15 @@
 #!/usr/bin/env python
 
+"""
+string diagrams in a bicategory.
+
+For the theory:
+https://stringdiagram.com/wp-content/uploads/2024/08/graphicaltheoryofmonadsv2.0.pdf
+
+(note to self. previous version: RiemannCodes2022/bicategory.py)
+
+"""
+
 from string import ascii_lowercase, ascii_uppercase
 from functools import reduce
 import operator 
@@ -84,137 +94,6 @@ class HCell1(Cell1):
 
     def __getitem__(self, idx):
         return self.cells[idx]
-
-
-#class Matrix(object):
-#    """
-#    The set of paths in a diagram.
-#    This is both a matrix M (indexed by output,input),
-#    and a lattice: ordered set of paths, pairs of (left,right) with left < right.
-#    """
-#    def __init__(self, M, pairs=[]):
-#        M = [[(list(x) if type(x) is list else [x]) for x in row] for row in M]
-#        self.M = M
-#        self.shape = (len(M), len(M[0]))
-#        paths = []
-#        for row in M:
-#          for ps in row:
-#            assert type(ps) is list
-#            for p in ps:
-#                assert isinstance(p, Path)
-#                paths.append(p)
-#        s_paths = set(paths)
-#        top = bot = paths[0] # start here
-#        for l,r in pairs: # l < r
-#            assert isinstance(l, Path)
-#            assert isinstance(r, Path)
-#            assert l in s_paths
-#            assert r in s_paths
-#            assert l != r
-#        for l,r in list(pairs):
-#            for s,t in list(pairs):
-#                if s==r and (l,t) in pairs:
-#                    pairs.remove((l,t))
-#        while 1:
-#            for l,r in pairs: # l < r
-#                if l==top:
-#                    top = r
-#                    break
-#                if r==bot:
-#                    bot = l
-#                    break
-#            else:
-#                break
-#            continue
-#        self.paths = paths
-#        self.pairs = list(pairs)
-#        self.s_paths = s_paths
-#        self.top = top
-#        self.bot = bot
-#        assert (top != bot) or len(paths)==1, str(self)
-#    def __str__(self):
-#        s = [[len(ps) for ps in row] for row in self.M]
-#        return str(s)
-#    def dump(M):
-#        print("="*79)
-#        print("Matrix.dump", M)
-#        print("paths:", len(M.paths))
-#        letters = ascii_lowercase + ascii_uppercase
-#        paths = M.paths
-#        names = {paths[i]:letters[i] for i in range(len(paths))}
-#        s = ["["+','.join(''.join(names[p] for p in ps) or '_' for ps in row)+"]"
-#            for row in M.M]
-#        s = "["+', '.join(s)+"]"
-#        print(s)
-#        for (l,r) in M.pairs:
-#            print("\t%s < %s"%(names[l], names[r]))
-#        print("top =", names[M.top])
-#        print("bot =", names[M.bot])
-#        print("="*79)
-#
-#    def __getitem__(self, idx):
-#        if type(idx) is int:
-#            return list(self.M[idx])
-#        else:
-#            idx, jdx = idx
-#            return list(self.M[idx][jdx])
-#    def __mul__(self, other):
-#        #print("__mul__", self, other)
-#        lhs, rhs = self.M, other.M
-#        assert self.shape[1] == other.shape[0], (self.shape, other.shape)
-#        M = []
-#        lookup = {}
-#        for j in range(self.shape[0]):
-#            row = []
-#            for i in range(other.shape[1]): 
-#                paths = []
-#                for k in range(self.shape[1]):
-#                    left = self[j][k]
-#                    right = other[k][i]
-#                    for l in left:
-#                        for r in right:
-#                            p = l<<r
-#                            lookup[l,r] = p
-#                            paths.append(p)
-#                row.append(paths)
-#            M.append(row)
-#        pairs = []
-#        for (a,b) in self.pairs:
-#          for c in other.paths:
-#            if (a,c) in lookup and (b,c) in lookup:
-#                pairs.append( (lookup[a,c], lookup[b,c]) )
-#        for a in self.paths:
-#          for (b,c) in other.pairs:
-#            if (a,b) in lookup and (a,c) in lookup:
-#                pairs.append( (lookup[a,b], lookup[a,c]) )
-#        for (a,b) in self.pairs:
-#          for (c,d) in other.pairs:
-#            if (a,c) in lookup and (b,d) in lookup:
-#                pairs.append( (lookup[a,c], lookup[b,d]) )
-#        #print("pairs:", len(pairs))
-#        paths = Matrix(M, pairs)
-#        #print("\t", paths)
-#        return paths
-#    def __add__(self, other):
-#        M = []
-#        for i in range(self.shape[0]+other.shape[0]):
-#          row = []
-#          for j in range(self.shape[1]+other.shape[1]):
-#            if i < self.shape[0]:
-#                if j < self.shape[1]:
-#                    paths = list(self[i][j])
-#                else:
-#                    paths = []
-#            else:
-#                if j < self.shape[1]:
-#                    paths = []
-#                else:
-#                    paths = list(other[i-self.shape[0]][j-self.shape[1]])
-#            row.append(paths)
-#          M.append(row)
-#        pairs = self.pairs + other.pairs
-#        pairs.append((self.top, other.bot))
-#        return Matrix(M, pairs)
 
 
 class Lattice(object):
@@ -381,18 +260,18 @@ class Lattice(object):
 twos = lambda items : [(items[i], items[i+1]) for i in range(len(items)-1)]
 
 
-class Graph(object):
-    def __init__(self):
+class Visitor(object):
+    def __init__(self, cell, dia):
+        self.cell = cell
+        self.dia = dia
         self.lookup = {}
-        self.cvs = Canvas()
 
-    def __call__(self, item, **kw):
-        #print("Graph.__call__", type(item))
+    def on_visit(self, dia, **kw):
+        #print("Visitor.__call__", type(dia))
         lookup = self.lookup
-        cvs = self.cvs
-        if isinstance(item, diagram.Spider):
-            o_paths = [p.backwards() for p in item.trace["top"]]
-            i_paths = [p.backwards() for p in item.trace["bot"]]
+        if isinstance(dia, diagram.Spider):
+            o_paths = [p.backwards() for p in dia.trace["top"]]
+            i_paths = [p.backwards() for p in dia.trace["bot"]]
             i_ports = []
             o_ports = []
             if o_paths and i_paths:
@@ -408,17 +287,78 @@ class Graph(object):
                 i_ports = [[p] for p in i_paths]
                 pairs = twos(i_paths)
             else:
-                assert 0, "empty spider: %s?"%item
-            #print("Graph.__call__:", len(o_ports), len(i_ports))
+                assert 0, "empty spider: %s?"%dia
+            #print("Visitor.__call__:", len(o_ports), len(i_ports))
             M = Lattice(o_ports, i_ports, pairs)
 
-        elif isinstance(item, diagram.VDia):
-            M = reduce(operator.mul, [lookup[child] for child in item])
-        elif isinstance(item, diagram.HDia):
-            M = reduce(operator.add, [lookup[child] for child in item])
+        elif isinstance(dia, diagram.VDia):
+            M = reduce(operator.mul, [lookup[child] for child in dia])
+        elif isinstance(dia, diagram.HDia):
+            M = reduce(operator.add, [lookup[child] for child in dia])
         else:
-            assert 0, type(item)
-        lookup[item] = M
+            assert 0, type(dia)
+        lookup[dia] = M
+
+    def process(self):
+        cell = self.cell
+        dia = self.dia
+        lookup = self.lookup
+        if 0:
+            bg = box.WeakFillBox([0.9*white])
+            dia = box.OBox([bg, dia])
+            dia.strict = True
+        cvs = dia.render(refresh=False)
+        dia.visit(self.on_visit)
+
+        llx, lly = dia.llx, dia.lly
+        urx, ury = dia.urx, dia.ury
+        cvs.stroke(path.rect(dia.llx, dia.lly, dia.width, dia.height), [grey])
+
+        #x0, y0 = dia.llx, dia.lly
+        #x1, y1 = dia.urx, dia.ury
+
+        M = lookup[dia]
+        #M.dump()
+        sup, inf = M.sup, M.inf
+
+        if len(M.o_ports) == 0 or len(M.i_ports) == 0:
+            return cvs
+
+        o_port = M.o_ports[0]
+        i_port = M.i_ports[0]
+
+        for p0 in i_port:
+            if p0 in inf:
+                break
+
+        #p0 = path.path([path.moveto(x0, y0), path.lineto(*p0.getat(0))]) >> p0
+        x0, y0 = p0.getat(0)
+        x1, y1 = p0.getat(1)
+        left = mkpath([(x0,y0),(llx,lly),(llx,ury),(x1,y1)])
+
+        p0 = left >> p0.backwards()
+        print(cell.src, cell.tgt)
+        
+        cvs.stroke(p0, [red]+st_arrow)
+
+        if 0:
+            # debug lattice structure
+            bg = cvs
+            cvs = Canvas()
+            bb = bg.get_bound_box()
+            x = 0
+            for p in M.paths:
+                fg = Canvas(bg)
+                if p in sup:
+                    fg.stroke(p, [red]+st_arrow)
+                elif p in inf:
+                    fg.stroke(p, [blue]+st_arrow)
+                else:
+                    fg.stroke(p, [grey]+st_Thick+st_arrow)
+                cvs.insert(x, 0, fg)
+                x += bb.width+0.2
+
+        return cvs
 
 
 class Cell2(object):
@@ -436,40 +376,9 @@ class Cell2(object):
         return str(self) == str(other)
 
     def construct(self):
-        item = self.on_construct()
-        if 0:
-            bg = box.WeakFillBox([0.9*white])
-            item = box.OBox([bg, item])
-            item.strict = True
-        cvs = item.render(refresh=False)
-        graph = Graph()
-        item.visit(graph)
-        #cvs = Canvas([cvs, graph.process()])
-        cvs.stroke(path.rect(item.llx, item.lly, item.width, item.height), [grey])
-        lookup = graph.lookup
-        #print("Cell2.construct", len(lookup))
-        M = lookup[item]
-
-        #print("Cell2.construct")
-        #M.dump()
-
-        sup, inf = M.sup, M.inf
-
-        bg = cvs
-        cvs = Canvas()
-        bb = bg.get_bound_box()
-        x = 0
-        for p in M.paths:
-            fg = Canvas(bg)
-            if p in sup:
-                fg.stroke(p, [red]+st_arrow)
-            elif p in inf:
-                fg.stroke(p, [blue]+st_arrow)
-            else:
-                fg.stroke(p, [grey]+st_Thick+st_arrow)
-            cvs.insert(x, 0, fg)
-            x += bb.width+0.2
-
+        dia = self.on_construct()
+        visitor = Visitor(self, dia)
+        cvs = visitor.process()
         return cvs
 
     def on_construct(self): # override me
@@ -516,6 +425,7 @@ class HCell2(Compound2):
         dias = [a.on_construct() for a in self.cells]
         dia = diagram.HDia(dias)
         dia.name = self.name
+        dia.cell = self
         return dia
 
 class VCell2(Compound2):
@@ -523,6 +433,7 @@ class VCell2(Compound2):
         dias = [a.on_construct() for a in self.cells]
         dia = diagram.VDia(dias)
         dia.name = self.name
+        dia.cell = self
         return dia
 
 
@@ -540,10 +451,11 @@ class Spider(Cell2):
         tgt, src = self.tgt, self.src
         top_attrs = [a.st for a in tgt]
         bot_attrs = [a.st for a in src]
-        spider = diagram.Spider(len(src), len(tgt), 
+        dia = diagram.Spider(len(src), len(tgt), 
             top_attrs=top_attrs, bot_attrs=bot_attrs, pip=self.pip, **self.kw)
-        spider.name = self.name
-        return spider
+        dia.name = self.name
+        dia.cell = self
+        return dia
 
 
 class Swap(Cell2):
@@ -554,6 +466,7 @@ class Swap(Cell2):
         X, Y = self.cells
         dia = diagram.Relation(2, 2, topbot=[(1,0), (0,1)], bot_attrs=[X.st, Y.st])
         dia.name = self.name
+        dia.cell = self
         return dia
 
 class Space(Cell2):
@@ -563,6 +476,7 @@ class Space(Cell2):
     def on_construct(self):
         dia = diagram.Spider(0, 0, min_width=self.width)
         dia.name = self.name
+        dia.cell = self
         return dia
 
 
@@ -606,43 +520,45 @@ def test():
     #op = ((comul*mul*comul) << (counit*mul)) * (comul << X)
 
     #op = (comul << mul)
-    op = mul * comul
-    op = comul * mul * comul
-    op = ((mul*comul) << Xi) * comul
-    op = op << Xi
-    op = mul * (mul << Xi) * op * comul
-    op = (mul << Xi) * (Xi << comul)
-    #op = op << op
-    #op = Spider(X, XXXX) * op * Spider(XXXX, X)
 
-    #op = mul << comul
-    #op = mul * comul
-    #op = (mul * comul * mul * comul) << ((comul<<Xi)*comul)
-
-    #op = counit << counit << unit
-    op = counit * unit
-    #op = counit * mul
-    #op = comul * unit
-
-    #op = mul << (unit * counit)
     gap = unit*counit
     bone = counit*unit
 
-    op = mul * (Xi << gap)
-    #op = mul * (Xi << unit)
-    op = mul * (mul << (unit * counit))
-    op = op * (Xi << comul)
-    op = mul * (op << gap) * (Xi << comul)
+    cvs = Canvas()
+    x = 0.
+    for op in [
+        mul * comul,
+        comul * mul * comul,
+        ((mul*comul) << Xi) * comul,
+        #op << Xi,
+        #mul * (mul << Xi) * op * comul,
+        (mul << Xi) * (Xi << comul),
+        #counit << counit << unit,
+        counit * unit,
+        #counit * mul,
+        #comul * unit,
+        #mul << (unit * counit),
+        mul * (Xi << gap),
+        #mul * (Xi << unit),
+        mul * (mul << (unit * counit)),
+        #op * (Xi << comul),
+        #mul * (op << gap) * (Xi << comul),
+    ]:
+        fg = op.construct()
+        cvs.insert(x, 0, fg)
+        x += fg.get_bound_box().width + 0.3
 
-    bubble = counit * mul * comul * unit
-    op = bubble << bubble
+    #bubble = counit * mul * comul * unit
+    #op = bubble << bubble
 
-    save("spider-test", op)
+    save("spider-test", cvs)
 
 
 
 if __name__ == "__main__":
     test()
+
+    print("OK\n\n\n\n")
 
 
 
